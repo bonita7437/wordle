4f0c0138b526c889eeb574cd4bae2c18c04a5f79
hyeran.lee <bonita7437@hanmail.net>
2024-06-23 18:50:02 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function appStart() {
     const div = document.createElement("div");
     div.innerText = "게임이 종료됐습니다.";
     div.style =
-      "display:flex; justify-content:center; align-items:center; position:absolute; top:36vh; left:40vw; background-color:white; width:200px; height:50px;";
+      "display:flex; justify-content:center; align-items:center; position:absolute; top:36vh; left:60vw; background-color:white; width:200px; height:50px;";
     document.body.appendChild(div);
   };
 
@@ -27,6 +27,7 @@ function appStart() {
 
   const handleEnterKey = () => {
     let 맞은_갯수 = 0;
+
     for (let i = 0; i < 5; i++) {
       const block = document.querySelector(
         `.board-column[data-index='${attempts}${i}']`
@@ -38,7 +39,6 @@ function appStart() {
         block.style.background = "#6AAA64";
       } else if (정답.includes(입력한_글자)) block.style.background = "#C9B458";
       else block.style.background = "#787C7E";
-
       block.style.color = "white";
     }
 
@@ -59,12 +59,11 @@ function appStart() {
   const handleKeydown = (event) => {
     const key = event.key.toUpperCase();
     const keyCode = event.keyCode;
-    console.log(event.key, event.keyCode);
     const thisBlock = document.querySelector(
       `.board-column[data-index='${attempts}${index}']`
     );
 
-    if (event.key === "backspace") handleBackspace();
+    if (event.key === "Backspace") handleBackspace();
     else if (index === 5) {
       if (event.key === "Enter") handleEnterKey();
       else return;
@@ -77,19 +76,19 @@ function appStart() {
   const startTimer = () => {
     const 시작_시간 = new Date();
 
-    function setTime() {
+    function settime() {
       const 현재_시간 = new Date();
       const 흐른_시간 = new Date(현재_시간 - 시작_시간);
       const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
       const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
-      const timeDiv = document.querySelector("#timer");
-      timeDiv.innerText = `${분}:${초}`;
+      const timediv = document.querySelector("#timer");
+      timediv.innerText = `${분}:${초}`;
     }
-    timer = setInterval(setTime, 1000);
+
+    timer = setInterval(settime, 1000);
   };
 
   startTimer();
-
   window.addEventListener("keydown", handleKeydown);
 }
 
